f3cc4008e216d444c9d034e46142475be0a88022
Marc Mansour <marcm9387@gmail.com>
2025-02-17 21:01:06 +0100

    Remove rounded border from logo in Navbar

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -16,13 +16,7 @@ export default function Navbar() {
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
         <Link href="/resumes" className="flex items-center gap-2">
-          <Image
-            src={logo}
-            alt="Logo"
-            width={35}
-            height={35}
-            className="rounded-full"
-          />
+          <Image src={logo} alt="Logo" width={35} height={35} className="" />
           <span className="text-xl font-bold tracking-tight">
             Boring Resume
           </span>
