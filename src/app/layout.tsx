7f752a24d9eca55517d0e072240f292e0311a1c5
Marc Mansour <marcm9387@gmail.com>
2025-02-17 20:38:00 +0100

    Rename project from AI Resume Builder to Boring Resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - AI Resume Builder",
-    absolute: "AI Resume Builder",
+    template: "%s - Boring Resume",
+    absolute: "AI Boring Resume Builder",
   },
   description:
-    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
+    "Boring Resume is the easiest way to create a professional resume that will help you land your dream job.",
 };
 
 export default function RootLayout({
