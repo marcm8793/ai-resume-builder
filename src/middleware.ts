c89b6b5a4dde7b011f4f06ca0bd97acbbc7746a6
Marc Mansour <marcm9387@gmail.com>
2025-02-19 23:18:41 +0100

    Add Terms of Service route to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/stripe-webhook",
+  "/tos",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
