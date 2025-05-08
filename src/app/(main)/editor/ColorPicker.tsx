52083c22b7c249974d751c95a2ca883742ae52c0
Marc Mansour <marcm9387@gmail.com>
2025-05-08 09:16:03 +0200

    Enhance ColorPicker component by adding a custom color palette to the TwitterPicker

--- src/app/(main)/editor/ColorPicker.tsx
+++ src/app/(main)/editor/ColorPicker.tsx
@@ -45,7 +45,23 @@ export default function ColorPicker({ color, onChange }: ColorPickerProps) {
         className="border-none bg-transparent shadow-none"
         align="end"
       >
-        <TwitterPicker color={color} onChange={onChange} triangle="top-right" />
+        <TwitterPicker
+          color={color}
+          onChange={onChange}
+          triangle="top-right"
+          colors={[
+            "#FF6900",
+            "#FCB900",
+            "#7BDCB5",
+            "#00D084",
+            "#8ED1FC",
+            "#0693E3",
+            "#ABB8C3",
+            "#EB144C",
+            "#F78DA7",
+            "#000000",
+          ]}
+        />
       </PopoverContent>
     </Popover>
   );
